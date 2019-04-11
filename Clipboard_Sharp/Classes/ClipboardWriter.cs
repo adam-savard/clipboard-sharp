c5037fb4267e54da578c7aff28b5ebf998763792
savardfs <fanman93@gmail.com>
2019-04-10 21:02:39 -0300

    Add project files.

---
+++ Clipboard_Sharp/Classes/ClipboardWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clipboard_Sharp.Classes
+{
+	public class ClipboardWriter
+	{
+		public static void writeToFile(string content)
+		{
+			string file = DateTime.Now.ToString("hh-mm-ss-ffff") + ".txt";
+			string folder = DateTime.Now.ToString("yyyy-MM-dd");
+
+			string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments),"ClipboardSharp",folder);
+
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
+
+			File.WriteAllText(System.IO.Path.Combine(path, file), content);
+			
+		}
+	}
+}
---
+++ Clipboard_Sharp/frmClipboardSharp.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clipboard_Sharp.Classes;
+
+namespace Clipboard_Sharp
+{
+	public partial class frmClipboardSharp : Form
+	{
+		private bool allowshowdisplay = false;
+		ClipboardDetect dt;
+
+		static bool saveClips = true;
+
+		protected override void SetVisibleCore(bool value)
+		{
+			base.SetVisibleCore(allowshowdisplay ? value : allowshowdisplay);
+		}
+		public frmClipboardSharp()
+		{
+			InitializeComponent();
+			dt = new ClipboardDetect();
+		}
+
+		
+
+		private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		
+
+		private void saveToggle_Click(object sender, EventArgs e)
+		{
+			if (saveClips)
+			{
+				saveClips = !saveClips;
+				dt.Stop();
+			}
+			else
+			{
+				saveClips = !saveClips;
+				dt = new ClipboardDetect();
+			}
+		}
+
+		private void delOldClips_Click(object sender, EventArgs e)
+		{
+			System.IO.DirectoryInfo di = new DirectoryInfo(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDoc‌​uments), "ClipboardSharp"));
+
+			foreach (FileInfo file in di.GetFiles())
+			{
+				file.Delete();
+			}
+			foreach (DirectoryInfo dir in di.GetDirectories())
+			{
+				dir.Delete(true);
+			}
+		}
+	}
+}
