7ad6c15f8bf1bccb1c758b9865a2b6b5f3447cae
savardfs <fanman93@gmail.com>
2019-04-10 21:02:39 -0300

    Add project files.

---
+++ Clipboard_Sharp/Classes/ClipboardDetect.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Clipboard_Sharp.Classes
+{
+	public class ClipboardDetect
+	{
+		static System.Timers.Timer scanForClipboardChange;
+		static string currentData = "";
+		static bool displayCopied = false;
+
+		public static bool DisplayCopied { get => displayCopied; set => displayCopied = value; }
+
+		public ClipboardDetect()
+		{
+			Start();
+		}
+
+		public void Start()
+		{
+			scanForClipboardChange = new System.Timers.Timer(200);
+			scanForClipboardChange.Elapsed += CheckClipboardChange;
+			scanForClipboardChange.AutoReset = true;
+			scanForClipboardChange.Enabled = true;
+			scanForClipboardChange.Start();
+		}
+
+		public void Stop()
+		{
+			scanForClipboardChange.Stop();
+		}
+
+		public static void chk()
+		{
+			string currentClip = Clipboard.GetText();
+			if (currentClip != "")
+			{
+				if (!currentClip.Equals(currentData))
+				{
+					currentData = currentClip;
+					DisplayCopied = true;
+					ClipboardWriter.writeToFile(currentData);
+				}
+			}
+		}
+		
+		private static void CheckClipboardChange(Object source, ElapsedEventArgs e)
+		{
+			Thread thread = new Thread(() => chk());
+			thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+			thread.Start();
+			thread.Join();
+
+			
+		}
+
+	}
+}
---
+++ Clipboard_Sharp/frmClipboardSharp.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Clipboard_Sharp
+{
+	partial class frmClipboardSharp
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClipboardSharp));
+			this.mnuClipSharp = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToggle = new System.Windows.Forms.ToolStripMenuItem();
+			this.delOldClips = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.icoSysTray = new System.Windows.Forms.NotifyIcon(this.components);
+			this.mnuClipSharp.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// mnuClipSharp
+			// 
+			this.mnuClipSharp.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.mnuClipSharp.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.saveToggle,
+            this.delOldClips,
+            this.exitToolStripMenuItem1});
+			this.mnuClipSharp.Name = "mnuClipSharp";
+			this.mnuClipSharp.Size = new System.Drawing.Size(211, 128);
+			// 
+			// showToolStripMenuItem
+			// 
+			this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+			this.showToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+			this.showToolStripMenuItem.Text = "Open Saved Clips";
+			// 
+			// saveToggle
+			// 
+			this.saveToggle.Name = "saveToggle";
+			this.saveToggle.Size = new System.Drawing.Size(210, 24);
+			this.saveToggle.Text = "Stop Saving Clips";
+			this.saveToggle.Click += new System.EventHandler(this.saveToggle_Click);
+			// 
+			// delOldClips
+			// 
+			this.delOldClips.Name = "delOldClips";
+			this.delOldClips.Size = new System.Drawing.Size(210, 24);
+			this.delOldClips.Text = "Delete Old Clips";
+			this.delOldClips.Click += new System.EventHandler(this.delOldClips_Click);
+			// 
+			// exitToolStripMenuItem1
+			// 
+			this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+			this.exitToolStripMenuItem1.Size = new System.Drawing.Size(210, 24);
+			this.exitToolStripMenuItem1.Text = "Exit";
+			this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+			// 
+			// icoSysTray
+			// 
+			this.icoSysTray.ContextMenuStrip = this.mnuClipSharp;
+			this.icoSysTray.Icon = ((System.Drawing.Icon)(resources.GetObject("icoSysTray.Icon")));
+			this.icoSysTray.Text = "Clipboard#";
+			this.icoSysTray.Visible = true;
+			// 
+			// frmClipboardSharp
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "frmClipboardSharp";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.mnuClipSharp.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ContextMenuStrip mnuClipSharp;
+		private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToggle;
+		private System.Windows.Forms.NotifyIcon icoSysTray;
+		private System.Windows.Forms.ToolStripMenuItem delOldClips;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+	}
+}
+
